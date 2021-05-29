688d52a88a29eb33e378fb7c9f05e7dd51151263
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-05-29 13:19:20 +0200

    first commit

---
+++ src/main/java/ch/resttest/Amigoscode2Application.java
@@ -0,0 +1,13 @@
+package ch.resttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amigoscode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amigoscode2Application.class, args);
+	}
+
+}
