2c7145e2474180f88a57128218a2a5c46820a245
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-05-30 15:30:54 +0200

    IImplemented Post Request to set new students

--- src/main/java/ch/resttest/student/StudentRepository.java
+++ src/main/java/ch/resttest/student/StudentRepository.java
@@ -1,6 +1,9 @@
 package ch.resttest.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -10,5 +13,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
 	
+	/*
+	 * Will be transformed in an SQL like 
+	 * SELECT * FROM student where emeil = param
+	 * JPAQL und nicht SQL
+	 * */
+	@Query("SELECT s FROM Student s WHERE s.email = ?1")
+	Optional<Student> findStudentByEnail(String emailString);
 
 }
--- src/main/java/ch/resttest/student/StudentService.java
+++ src/main/java/ch/resttest/student/StudentService.java
@@ -3,6 +3,7 @@ package ch.resttest.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,6 +23,17 @@ public class StudentService {
 	public List<Student> getStudentExample()
 	{
 		return studentRepository.findAll();
+	}
+
+
+	public void addNewStudent(Student student) {
+		// TODO Auto-generated method stub
+		Optional<Student> studentOptional = studentRepository.findStudentByEnail(student.getEmail());
+		if(studentOptional.isPresent()) {
+			throw new IllegalStateException("email taken");
+			
+		}
+		studentRepository.save(student);
 	}
 	
 }
